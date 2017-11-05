2f86ed06dfe257ac43610bd8af1a4a12f4ec2ad7
harshil1793 <harshil1793@gmail.com>
2017-11-05 16:12:36 -0500

    feat: Material, routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+        MatButtonModule,
+        MatCheckboxModule,
+        MatToolbarModule
+      } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
+import { ContactComponent } from './contact/contact.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true }),
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
+import { ContactComponent } from './contact/contact.component'
+
+export const rootRouterConfig: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
